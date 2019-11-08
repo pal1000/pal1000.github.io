83334d7e6ab13efaef0eecef626b89534ee0071b
pal1000 <liviuprodea@yahoo.com>
2019-11-08 12:20:32 +0200

    mesa-dist-win: Remove experimental meson option. It's dead.

--- shared/development/mesa-dist-win.cmd
+++ shared/development/mesa-dist-win.cmd
@@ -23,33 +23,27 @@ git clone https://github.com/pal1000/mesa-dist-win.git mesa-dist-win
 @echo -----------------------
 @echo What do you want to do?
 @echo -----------------------
-@echo 1. Build Mesa3D (normal mode)
-@echo 2. Build Mesa3D (experimental Meson support)
-@echo 3. Update local repository
-@echo 4. Update remote repository
-@echo 5. Launch GIT GUI
-@echo 6. Wipe all uncommited changes
-@echo 7. Insert commands manually
-@echo 8. Exit
+@echo 1. Build Mesa3D
+@echo 2. Update local repository
+@echo 3. Update remote repository
+@echo 4. Launch GIT GUI
+@echo 5. Wipe all uncommited changes
+@echo 6. Insert commands manually
+@echo 7. Exit
 @set choice=0
 @set /p choice="Enter your Choice here:"
 @if "%choice%"=="1" GOTO Build_mesa
-@if "%choice%"=="2" GOTO experimental
-@if "%choice%"=="3" GOTO Update_local
-@if "%choice%"=="4" GOTO Update_remote
-@if "%choice%"=="5" GOTO GUI
-@if "%choice%"=="6" GOTO wipe_uncommited_changes
-@if "%choice%"=="7" GOTO Command
-@if "%choice%"=="8" GOTO Exit
+@if "%choice%"=="2" GOTO Update_local
+@if "%choice%"=="3" GOTO Update_remote
+@if "%choice%"=="4" GOTO GUI
+@if "%choice%"=="5" GOTO wipe_uncommited_changes
+@if "%choice%"=="6" GOTO Command
+@if "%choice%"=="7" GOTO Exit
 @echo Invaild entry
 @GOTO Choice
 
 :Build_mesa
 @start buildscript\build.cmd
-@GOTO Choice
-
-:experimental
-@start buildscript\build.cmd /enablemeson
 @GOTO Choice
 
 :Update_local
