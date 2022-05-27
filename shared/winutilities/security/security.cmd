65f4543ddcaaa68b90895c3d58601f08725d8e1f
pal1000 <liviuprodea@yahoo.com>
2022-05-27 12:11:55 +0300

    Backup 27/05/2022

--- shared/winutilities/security/security.cmd
+++ shared/winutilities/security/security.cmd
@@ -11,6 +11,16 @@
 @netsh interface teredo set state disabled
 @netsh interface isatap set state disabled
 @netsh winhttp reset proxy
+@netsh advfirewall firewall add rule name="RPC TCP" protocol=TCP dir=out localport=135 action=block enable=yes
+@netsh advfirewall firewall add rule name="RPC UDP" protocol=UDP dir=out localport=135 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS NS TCP" protocol=TCP dir=out localport=137 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS NS UDP" protocol=UDP dir=out localport=137 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS DS TCP" protocol=TCP dir=out localport=138 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS DS UDP" protocol=UDP dir=out localport=138 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS SS TCP" protocol=TCP dir=out localport=139 action=block enable=yes
+@netsh advfirewall firewall add rule name="NetBIOS SS UDP" protocol=UDP dir=out localport=139 action=block enable=yes
+@netsh advfirewall firewall add rule name="Microsoft DS TCP" protocol=TCP dir=out localport=445 action=block enable=yes
+@netsh advfirewall firewall add rule name="Microsoft DS UDP" protocol=UDP dir=out localport=445 action=block enable=yes
 @pause
 
 
