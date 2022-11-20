78368f08de801c96690b36004bc21aff67c73114
pal1000 <liviuprodea@yahoo.com>
2022-11-21 00:23:00 +0200

    Backup 21/11/2022

---
+++ shared/winutilities/optimize/disable-volume-shadow-copy.cmd
@@ -0,0 +1,12 @@
+@echo off
+@cd /d "%~dp0"
+@set "ERRORLEVEL="
+@CMD /C EXIT 0
+@"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@powershell -Command Start-Process ""%0"" -Verb runAs 2>nul
+@exit
+)
+@net stop swprv
+@sc config swprv start=disabled
+@pause
