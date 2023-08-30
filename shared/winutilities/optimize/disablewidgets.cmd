4ab766e1f088b1ddf763dc45531c80c4be65186c
pal1000 <liviuprodea@yahoo.com>
2023-08-30 12:31:20 +0300

    Backup 30/08/2023

--- shared/winutilities/optimize/disablewidgets.cmd
+++ shared/winutilities/optimize/disablewidgets.cmd
@@ -1,2 +1,12 @@
-@winget uninstall "windows web experience pack"
-@pause
+@cd /d "%~dp0"
+@set "ERRORLEVEL="
+@CMD /C EXIT 0
+@"%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" >nul 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@powershell -Command Start-Process ""%0"" -Verb runAs 2>nul
+@exit
+)
+:--------------------------------------
+@echo Get-AppxPackage -AllUsers ^^^| Where-Object {$_.Name -like "*WebExperience*"} ^^^| Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue | powershell -NoLogo
+@ecmd /c echo.
+@cmd /c pause
