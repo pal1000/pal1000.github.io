bc6fa4f9fe624492b4d09b54a4791e7a828e3670
pal1000 <liviuprodea@yahoo.com>
2023-09-23 12:29:09 +0300

    Streamm downloader: Fix ordered list

--- shared/experiments/TV Online/thetvappto/index.php
+++ shared/experiments/TV Online/thetvappto/index.php
@@ -1,9 +1,10 @@
 <?php
 $source=file_get_contents("https://185.155.103.23/tv",false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false]]));
 $strposstart=strpos($source,"<ol ");
-$strposend=strpos($source,'</ol>',$strposstart);
+$strposend=strpos($source,'</ol>',$strposstart)+strlen('</ol>');
 $source=substr($source,$strposstart,$strposend-$strposstart);
 $source=str_replace('/tv/','thetvappto.php?chname=',$source);
-$source=str_replace('</a>','</a></br/>',$source);
-echo $source.'</ol>';
+$source=str_replace('<a ','<li><a ',$source);
+$source=str_replace('</a>','</a></li>',$source);
+echo $source;
 ?>
