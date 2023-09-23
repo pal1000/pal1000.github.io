d8cf73e435fea7ad9081ab5ca0f8db45f1a24b06
pal1000 <liviuprodea@yahoo.com>
2023-09-23 09:17:04 +0300

    Stream downloader update

--- shared/experiments/TV Online/thetvappto/thetvappto.php
+++ shared/experiments/TV Online/thetvappto/thetvappto.php
@@ -1,5 +1,5 @@
 <?php
-$source=file_get_contents("https://thetvapp.to/tv/".$_GET['chname']);
+$source=file_get_contents("https://185.155.103.23/tv/".$_GET['chname'],false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false]]));
 $strposstart=strpos($source,"csrf-token");
 $strposstart=strpos($source,"content",$strposstart)+strlen("content")+2;
 $strposend=strpos($source,'"',$strposstart);
@@ -13,5 +13,5 @@ $cookie_1=substr(print_r($http_response_header,true),$strposstart,$strposend-$st
 $strposstart=strpos(print_r($http_response_header,true),"tvnow_session",$strposend);
 $strposend=strpos(print_r($http_response_header,true),';',$strposstart);
 $cookie_2=substr(print_r($http_response_header,true),$strposstart,$strposend-$strposstart);
-header('Location: '.file_get_contents("https://thetvapp.to/token/".$chid,false,stream_context_create(['http'=>['method'=>"POST",'header'=>['X-CSRF-TOKEN: '.$token,'Cookie: '.$cookie_1.';'.$cookie_2]]])));
+header('Location: '.file_get_contents("https://185.155.103.23/token/".$chid,false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false],'http'=>['method'=>"POST",'header'=>['X-CSRF-TOKEN: '.$token,'Cookie: '.$cookie_1.';'.$cookie_2]]])));
 ?>
