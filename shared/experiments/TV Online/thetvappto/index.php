f3e02533bf153d72925b53d85f2eb53a67490488
pal1000 <liviuprodea@yahoo.com>
2023-11-14 14:36:15 +0200

    Backup 14/11/2023

--- shared/experiments/TV Online/thetvappto/index.php
+++ shared/experiments/TV Online/thetvappto/index.php
@@ -1,10 +1,25 @@
 <?php
-$source=file_get_contents("https://185.155.103.23/tv",false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false]]));
+$source=file_get_contents("https://thetvapp.to/tv");
 $strposstart=strpos($source,"<ol ");
-$strposend=strpos($source,'</ol>',$strposstart)+strlen('</ol>');
+$strposstart=strpos($source,"<a ",$strposstart);
+$strposend=strpos($source,'</ol>',$strposstart);
 $source=substr($source,$strposstart,$strposend-$strposstart);
-$source=str_replace('/tv/','thetvappto.php?chname=',$source);
-$source=str_replace('<a ','<li><a ',$source);
-$source=str_replace('</a>','</a></li>',$source);
-echo $source;
+$source=substr($source,0,strlen($source)-strpos(strrev($source),">"));
+$source=explode("\n",$source);
+echo "#EXTM3U\n";
+$baseurl=(empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]";
+if($_SERVER['SERVER_PORT']!=80&&$_SERVER['SERVER_PORT']!=443) $baseurl.=":".$_SERVER['SERVER_PORT'];
+$baseurl.=htmlentities($_SERVER['SCRIPT_NAME']);
+$baseurl=substr($baseurl,0,strlen($baseurl)-strlen('index.php'));
+for($i=0;$i<count($source);$i++){
+  $minsrc=str_replace("'",'"',$source[$i]);
+  $strposstart=strpos($minsrc,'href="')+strlen('href="');
+  $strposend=strpos($minsrc,'"',$strposstart);
+  $chlink=substr($minsrc,$strposstart,$strposend-$strposstart);
+  $strposstart=strpos($minsrc,'>',$strposend)+1;
+  $strposend=strpos($minsrc,'<',$strposstart);
+  $chlabel=substr($minsrc,$strposstart,$strposend-$strposstart);
+  echo "#EXTINF:-1,".$chlabel." (thetvapp.to)\n".$baseurl.str_replace("/","",str_replace('/tv/','thetvappto.php?chname=',$chlink));
+  if(count($source)-$i>1) echo "\n\n";
+}
 ?>
--- shared/experiments/TV Online/thetvappto/thetvappto.php
+++ shared/experiments/TV Online/thetvappto/thetvappto.php
@@ -1,17 +1,11 @@
 <?php
-$source=file_get_contents("https://185.155.103.23/tv/".$_GET['chname'],false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false]]));
-$strposstart=strpos($source,"csrf-token");
-$strposstart=strpos($source,"content",$strposstart)+strlen("content")+2;
-$strposend=strpos($source,'"',$strposstart);
-$token=substr($source,$strposstart,$strposend-$strposstart);
-$strposstart=strpos($source,"/token/",$strposend)+strlen("/token/");
-$strposend=strpos($source,'"',$strposstart);
-$chid=substr($source,$strposstart,$strposend-$strposstart);
-$strposstart=strpos(print_r($http_response_header,true),"XSRF-TOKEN");
-$strposend=strpos(print_r($http_response_header,true),';',$strposstart);
-$cookie_1=substr(print_r($http_response_header,true),$strposstart,$strposend-$strposstart);
-$strposstart=strpos(print_r($http_response_header,true),"tvnow_session",$strposend);
-$strposend=strpos(print_r($http_response_header,true),';',$strposstart);
-$cookie_2=substr(print_r($http_response_header,true),$strposstart,$strposend-$strposstart);
-header('Location: '.file_get_contents("https://185.155.103.23/token/".$chid,false,stream_context_create(['ssl'=>["verify_peer"=>false,"verify_peer_name"=>false],'http'=>['method'=>"POST",'header'=>['X-CSRF-TOKEN: '.$token,'Cookie: '.$cookie_1.';'.$cookie_2]]])));
+$source=file_get_contents("https://thetvapp.to/tv/".$_GET['chname']);
+$strposstart=strpos($source,'source:');
+if($strposstart===false) $strposstart=strpos($source,'file:');
+$strposstart=strpos($source,'http',$strposstart);
+$strposend=strpos($source,',',$strposstart)-1;
+header('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0');
+header('Origin: https://thetvapp.to');
+header('Referer: https://thetvapp.to/');
+header('Location: '.substr($source,$strposstart,$strposend-$strposstart));
 ?>
